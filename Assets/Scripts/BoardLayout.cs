1bdaa27a598d3365670d3e48daf9274698472c4b
Pau Garrido <pau.garrido.7e3@itb.cat>
2021-04-29 12:40:42 +0200

    Grid Movement (First commit)
    Create the base of our grid movement

---
+++ Assets/Scripts/BoardLayout.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[CreateAssetMenu(menuName = "Scriptable Objects/Board/Layout")]
+public class BoardLayout : ScriptableObject
+{
+   [Serializable]
+   private class BoardSquareSetup
+    {
+        public Vector2Int position;
+        public Characters characterType;
+        public Teams teamType;
+    }
+
+    [SerializeField] private BoardSquareSetup[] boardSquare;
+
+    public int GetCharactersCount()
+    {
+        return boardSquare.Length;
+    }
+
+    public Vector2Int GetSquareCoordsAtIndex (int index)
+    {
+        if(boardSquare.Length <= index)
+        {
+            Debug.LogError("Index of character is out of range");
+            return new Vector2Int(-1, -1);
+        }
+        return new Vector2Int(boardSquare[index].position.x - 1, boardSquare[index].position.y - 1);
+    }
+
+    public string GetSquareCharacterNameAtIndex (int index)
+    {
+        if (boardSquare.Length <= index)
+        {
+            Debug.LogError("Index of character is out of range");
+            return "";
+        }
+        return boardSquare[index].characterType.ToString();
+    }
+
+    public Teams GetSquareTeamTypeAtIndex(int index)
+    {
+        if (boardSquare.Length <= index)
+        {
+            Debug.LogError("Index of character is out of range");
+            return Teams.Enemy;
+        }
+        return boardSquare[index].teamType;
+    }
+}
---
+++ Assets/Scripts/Characters/EnemyCharacter.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCharacter : Character
+{
+    private Vector2Int[] directions = new Vector2Int[] {
+
+        new Vector2Int(1, 1),
+        new Vector2Int(1, -1),
+        new Vector2Int(-1, 1),
+        new Vector2Int(-1, -1),
+        Vector2Int.left,
+        Vector2Int.up,
+        Vector2Int.right,
+        Vector2Int.down };
+
+    public override List<Vector2Int> SelectAvaliableSquares()
+    {
+        avaliableMoves.Clear();
+        float range = Board.BOARD_SIZE;
+        foreach (var direction in directions)
+        {
+            for (int i = 1; i <= range; i++)
+            {
+                Vector2Int nextCoords = occupiedSquare + direction * i;
+                Character character = board.GetCharacterOnSquare(nextCoords);
+                if (!board.CheckIfCoordinatedAreOnBoard(nextCoords))
+                    break;
+                if (character == null)
+                    TryToAddMove(nextCoords);
+                else if (!character.isFromSameTeam(this))
+                {
+                    TryToAddMove(nextCoords);
+                    break;
+                }
+                else if (character.isFromSameTeam(this))
+                    break;
+            }
+        }
+        return avaliableMoves;
+    }
+}
