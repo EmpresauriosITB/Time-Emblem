f9a827becc72356a8b62bebb18255cb285c661b0
paugarrido <pau.garrido.7e3@itb.cat>
2021-05-04 17:02:37 +0200

    MenuController Test

---
+++ Assets/Scripts/Menus/SpecialMenu/ActionsMenu.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionsMenu : MonoBehaviour
+{
+    public void OnClick_Back()
+    {
+        MenuManager.OpenMenu(Menu.Game_Menu, gameObject);
+    }
+}
---
+++ Assets/Scripts/Menus/SpecialMenu/StatsMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatsMenu : MonoBehaviour
+{
+
+    public Text HP;
+    public Character character;
+
+
+    private void Start()
+    {
+        setTextStats();
+    }
+
+
+
+    public void OnClick_Back()
+    {
+        MenuManager.OpenMenu(Menu.Game_Menu, gameObject);
+    }
+
+
+    public void setTextStats()
+    {
+   
+        HP.text = "HP: " + character.GetHp()
+ ;
+    }
+
+}
