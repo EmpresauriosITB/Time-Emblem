c5b622526614a6d2eefdf5cbf21ad589717753cd
RogerLario <roger.lario.7e3@itb.cat>
2021-05-07 14:11:55 +0200

    Revert "Merge branch 'developer' into design/art"
    This reverts commit bbe0863a67c969c7fea074558bca1826a4902c24.

--- Assets/Scripts/Characters/Abilities/AbilitySet.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "AbilitySet")]
-public class AbilitySet : ScriptableObject
-{
-    public List<AbilityCommon.AbiltiesId> abilities;
-}
