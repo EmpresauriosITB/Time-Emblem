d1cb537439fa79d41b3dd6ac428217abeb1733b8
paugarrido <pau.garrido.7e3@itb.cat>
2021-05-11 16:12:04 +0200

    Improve Player Movement

---
+++ Assets/Scripts/Player/PlayerFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour
+{
+    [SerializeField]
+    public Transform PlayerTransform;
+
+    private Vector3 _cameraOffset;
+
+    [Range(0.01f, 1.0f)]
+    public float SmoothFactor = 0.5f;
+
+    private void Start()
+    {
+        _cameraOffset = transform.position - PlayerTransform.position;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 newPos = PlayerTransform.position + _cameraOffset;
+
+        transform.position = Vector3.Slerp(transform.position, newPos, SmoothFactor);
+    }
+
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
-    public Transform camera;
+    
     public float Speed;
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
@@ -19,16 +19,17 @@ public class PlayerMovement : MonoBehaviour
     {
         float hor = Input.GetAxis("Horizontal");
         float ver = Input.GetAxis("Vertical");
-        Vector3 direction = new Vector3(hor, 0f, ver).normalized;
+        Vector3 direction = new Vector3(-hor, 0f, -ver).normalized;
 
         if(direction.magnitude >= 0.1f)
         {
    
-            //float targetAngle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg + camera.eulerAngles.y;
-            //float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            //transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
-            //Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float targetAngleV = Mathf.Atan(direction.y) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            Debug.Log($"TargetA: {targetAngle}; angle: {angle}");
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
 
             controller.Move(direction.normalized * Speed * Time.deltaTime);
         }
