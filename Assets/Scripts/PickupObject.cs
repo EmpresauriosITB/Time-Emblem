c057d3388acbeb292bf3162fe42417746d258331
Roger <roger.lario.7e3@itb.cat>
2021-05-17 19:00:03 +0200

    Drive boats and ships + leave boats and ships
    - Finished the pickupBoat script
    - Make that you can drive all boats and ships
    - Make that you can leave all the boats and ships
    - Changed some prefabs

--- Assets/Scripts/PickupObject.cs
+++ Assets/Scripts/PickupObject.cs
@@ -5,27 +5,50 @@ using UnityEngine;
 public class PickupObject : MonoBehaviour
 {
     public Camera pixelCamera;
+    public bool onBoard;
 
     public void Start()
     {
         pixelCamera = GameObject.FindObjectOfType<Camera>();
+        onBoard = false;
+    }
+
+    private void Update() {
+        exitBoat();
     }
 
     void OnTriggerStay(Collider collider)
     {
-        if (collider.gameObject.tag == "Player")
+        if (collider.gameObject.tag == "Player" && onBoard == false)
         {
             GameObject obj = collider.transform.GetChild(2).gameObject;
             obj.SetActive(true);
             if (Input.GetKeyDown(KeyCode.E))
             {
-                GameObject child = this.gameObject.transform.GetChild(0).gameObject;
+                GameObject child = this.gameObject.transform.GetChild(1).gameObject;
                 child.SetActive(true);
                 this.gameObject.GetComponent<PlayerController>().camara = collider.gameObject.GetComponent<PlayerController>().camara;
                 gameObject.GetComponent<PlayerController>().camara.transform.GetChild(0).GetComponent<PlayerFollow>().PlayerTransform = gameObject.transform;
                 gameObject.GetComponent<PlayerController>().setCanMove(true);
                 collider.gameObject.SetActive(false);
+                collider.gameObject.transform.SetParent(this.gameObject.transform.GetChild(0).gameObject.transform, true);
                 pixelCamera.fieldOfView = 60f;
+                StartCoroutine(waitBoard());
+            }
+        }
+    }
+
+    public void exitBoat() {
+        if (onBoard == true){
+            if(Input.GetKeyDown(KeyCode.E)){
+                onBoard = false;
+                pixelCamera.fieldOfView = 15f;
+                GameObject playerPirate = this.transform.GetChild(0).GetChild(0).gameObject;
+                playerPirate.SetActive(true);
+                playerPirate.transform.parent = null;
+                this.gameObject.transform.GetChild(1).gameObject.SetActive(false);
+                gameObject.GetComponent<PlayerController>().camara.transform.GetChild(0).GetComponent<PlayerFollow>().PlayerTransform = GameObject.Find("Player").transform;
+                gameObject.GetComponent<PlayerController>().setCanMove(false);
             }
         }
     }
@@ -37,5 +60,11 @@ public class PickupObject : MonoBehaviour
             GameObject obj = other.transform.GetChild(2).gameObject;
             obj.SetActive(false);
         }
+    }
+
+    IEnumerator waitBoard()
+    {
+        yield return new WaitForSeconds(1);
+        onBoard = true;
     }
 }
