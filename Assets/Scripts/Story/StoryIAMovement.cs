3271193b04485042051d31a262644138a268cee1
Pau Garrido <pau.garrido.7e3@itb.cat>
2021-05-20 16:35:25 +0200

    Story IA Movement

---
+++ Assets/Scripts/Story/StoryIAMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryIAMovement : MonoBehaviour
+{
+    public Transform[] points;
+    int currentPoint;
+    public float speed;
+
+    private void Start()
+    {
+        currentPoint = 0;
+    }
+
+    private void Update()
+    {
+        if (transform.position != points[currentPoint].position)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, points[currentPoint].position, speed * Time.deltaTime);
+        }
+        else
+            currentPoint = (currentPoint + 1) % points.Length;
+    }
+}
