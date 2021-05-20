8b26caa94e2236654d24ca50a59baae0d46c3cfa
Pau Garrido <pau.garrido.7e3@itb.cat>
2021-05-20 18:32:53 +0200

    Start IA Dialogs

---
+++ Assets/Scripts/Story/Dialogs/ControlDialogs.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ControlDialogs : MonoBehaviour
+{
+    private Animator anim;
+    private Queue<string> queueDialogs;
+    Texts texts;
+    [SerializeField] TextMeshProUGUI screenText;
+
+    public void ActivatePoster(Texts textObject)
+    {
+        anim.SetBool("BordeDialogo", true);
+        texts = textObject;
+    }
+
+    public void ActivateText()
+    {
+        queueDialogs.Clear();
+        foreach (string textSave in texts.arrayTexts)
+        {
+            queueDialogs.Enqueue(textSave);
+        }
+        NextSentence();
+    }
+
+    public void NextSentence()
+    {
+        if(queueDialogs.Count == 0)
+        {
+            ClosePoster();
+            return;
+        }
+
+        string currentSentence = queueDialogs.Dequeue();
+        screenText.text = currentSentence;
+    }
+
+    void ClosePoster()
+    {
+        anim.SetBool("BordeDialogo", false);
+    }
+
+}
---
+++ Assets/Scripts/Story/Dialogs/InteractableObjects.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObjects : MonoBehaviour
+{
+    public Texts texts;
+
+    private void OnTriggerStay(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                FindObjectOfType<ControlDialogs>().ActivatePoster(texts);
+            }
+        }
+    }
+}
