014ac6475ebdf984a7938ab7ffabe0e55694d445
larios-gin <roger.lario.7e3@itb.cat>
2021-05-24 01:08:33 +0200

    Intro text for zones
    - Added intro text for different zones into japan map
    - Created animations for each text
    - Created script for each activable zone

---
+++ Assets/Scripts/TextIntro/TextIntro.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextIntro : MonoBehaviour
+{
+    public Camera pixelCamera;
+    public GameObject textIntro;
+    Animation textAnim;
+
+    void Start()
+    {
+        pixelCamera = GameObject.FindObjectOfType<Camera>();
+        textIntro = GameObject.Find("TextIntro");
+        textAnim = textIntro.GetComponent<Animation>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            pixelCamera.fieldOfView = 30f;
+            textAnim.Play("TextPanel");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            pixelCamera.fieldOfView = 15f;
+            textAnim.Play("ExitZone");
+        }
+    }
+}
