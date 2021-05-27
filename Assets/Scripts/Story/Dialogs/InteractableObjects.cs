22cb072e35f7b8c866d93a7c056da95d0435d3ae
larios-gin <roger.lario.7e3@itb.cat>
2021-05-27 15:12:44 +0200

    Try to change dialogs when activate an event
    - Made 2 scripts to try to change the dialogs when a event appears (NOT WORKING)
    - Minor changes

--- Assets/Scripts/Story/Dialogs/InteractableObjects.cs
+++ Assets/Scripts/Story/Dialogs/InteractableObjects.cs
@@ -9,12 +9,18 @@ public class InteractableObjects : MonoBehaviour
     public GameObject Sensei;
     public GameObject partner;
     GameObject partner_character;
+    private Sensei1Text Sensei1Text;
+    private Sensei2Text Sensei2Text;
 
     private void Start()
     {
         Sensei = GameObject.Find("Sensei_Red_Temple");
         partner = GameObject.Find("Partner");
         partner_character = partner.transform.GetChild(0).gameObject;
+        Sensei1Text = FindObjectOfType<Sensei1Text>();
+        Sensei1Text.gameObject.SetActive(false);
+        Sensei2Text = FindObjectOfType<Sensei2Text>();
+        Sensei2Text.gameObject.SetActive(false);
     }
 
     private void OnTriggerStay(Collider collider)
@@ -42,6 +48,9 @@ public class InteractableObjects : MonoBehaviour
             {
                 partner.GetComponent<ActivateObject>().activatePartner();
                 this.GetComponent<StoryIAMovement>().enabled = false;
+                GetComponent<InteractableObjects>().enabled = false;
+                Sensei1Text.gameObject.SetActive(true);
+                
             }
             if (this.gameObject.Equals(partner_character)) {
                 partner.GetComponent<ActivateObject>().disableGuards();
---
+++ Assets/Scripts/Story/Dialogs/Sensei1Text.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sensei1Text : MonoBehaviour
+{
+    public Texts texts;
+    bool talking = false;
+    public GameObject Sensei;
+    public GameObject partner;
+    GameObject partner_character;
+    private Sensei2Text Sensei2Text;
+
+    private void Start()
+    {
+        Sensei = GameObject.Find("Sensei_Red_Temple");
+        partner = GameObject.Find("Partner");
+        partner_character = partner.transform.GetChild(0).gameObject;
+        Sensei2Text = FindObjectOfType<Sensei2Text>();
+        Sensei2Text.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerStay(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            GameObject obj = collider.transform.GetChild(2).gameObject;
+            obj.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                talking = true;
+                this.gameObject.GetComponent<StoryIAMovement>().Stop();
+                FindObjectOfType<ControlDialogs>().ActivatePoster(texts);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        GameObject obj = other.transform.GetChild(2).gameObject;
+        obj.SetActive(false);
+        if (talking == true)
+        {
+            if (this.gameObject.Equals(Sensei))
+            {
+                partner.GetComponent<ActivateObject>().activatePartner();
+                this.GetComponent<StoryIAMovement>().enabled = false;
+            }
+            if (this.gameObject.Equals(partner_character))
+            {
+                partner.GetComponent<ActivateObject>().disableGuards();
+                partner.GetComponent<ActivateObject>().activateEnemies();
+                partner.GetComponent<ActivateObject>().activatePartnerFollow();
+                GetComponent<Sensei1Text>().enabled = false;
+                Sensei2Text.gameObject.SetActive(true);
+            }
+            talking = false;
+            this.gameObject.GetComponent<StoryIAMovement>().Reload();
+            FindObjectOfType<ControlDialogs>().ClosePoster();
+        }
+    }
+}
