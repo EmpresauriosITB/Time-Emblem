bd5d9b4c86009ef4cfb994fe62c5d0bbadb42ea0
RogerLario <roger.lario.7e3@itb.cat>
2021-05-28 20:42:37 +0200

    Scene transitions + start battle event
    - Created transition animations (start - end)
    - Tested transitions when talk to an enemy to start battle
    - Start battle event
    - Created prefab for the transition canvas

--- Assets/Scripts/Story/Dialogs/InteractableObjects.cs
+++ Assets/Scripts/Story/Dialogs/InteractableObjects.cs
@@ -6,7 +6,8 @@ public class InteractableObjects : MonoBehaviour
 {
     public Texts texts;
     bool talking = false;
-    public GameObject Sensei, partner, partner_character, enemie1, enemie2, enemie3;
+    public GameObject Sensei, partner, partner_character, enemie1, enemie2, enemie3, sensei2;
+    LevelLoader battleLoader;
 
     private void Start()
     {
@@ -17,6 +18,8 @@ public class InteractableObjects : MonoBehaviour
         enemie1 = enemie.transform.GetChild(0).GetChild(0).gameObject;
         enemie2 = enemie.transform.GetChild(1).GetChild(0).gameObject;
         enemie3 = enemie.transform.GetChild(2).GetChild(0).gameObject;
+        sensei2 = enemie.transform.GetChild(3).GetChild(0).gameObject;
+        battleLoader = FindObjectOfType<LevelLoader>();
     }
 
     private void OnTriggerStay(Collider collider)
@@ -27,6 +30,10 @@ public class InteractableObjects : MonoBehaviour
             obj.SetActive(true);
             if (Input.GetKeyDown(KeyCode.E))
             {
+                if (this.gameObject.Equals(enemie1) || this.gameObject.Equals(enemie2) || this.gameObject.Equals(enemie3) || this.gameObject.Equals(sensei2))
+                {
+                    EventBattle();
+                }
                 talking = true;
                 this.gameObject.GetComponent<StoryIAMovement>().Stop();
                 FindObjectOfType<ControlDialogs>().ActivatePoster(texts);
@@ -44,32 +51,45 @@ public class InteractableObjects : MonoBehaviour
             {
                 EventActivatePartner();
             }
-            if (this.gameObject.Equals(partner_character)) {
+            if (this.gameObject.Equals(partner_character))
+            {
                 EventPartnerFollow();
             }
-            if (this.gameObject.Equals(enemie1) || this.gameObject.Equals(enemie2) || this.gameObject.Equals(enemie3)) {
+            if (this.gameObject.Equals(enemie1) || this.gameObject.Equals(enemie2) || this.gameObject.Equals(enemie3) || this.gameObject.Equals(sensei2))
+            {
                 EventBattle();
                 GameObject enemieBattle = this.transform.parent.gameObject;
                 enemieBattle.gameObject.SetActive(false);
             }
+
             talking = false;
             this.gameObject.GetComponent<StoryIAMovement>().Reload();
             FindObjectOfType<ControlDialogs>().ClosePoster();
         }
     }
 
-    public void EventActivatePartner() {
+    public void EventActivatePartner()
+    {
         partner.GetComponent<ActivateObject>().activatePartner();
         this.GetComponent<StoryIAMovement>().enabled = false;
     }
 
-    public void EventPartnerFollow() {
+    public void EventPartnerFollow()
+    {
         partner.GetComponent<ActivateObject>().disableGuards();
         partner.GetComponent<ActivateObject>().activateEnemies();
         partner.GetComponent<ActivateObject>().activatePartnerFollow();
     }
 
-    public void EventBattle() {
+    public void EventBattle()
+    {
+        StartCoroutine(waitBattle());
+    }
+
+    IEnumerator waitBattle()
+    {
+        yield return new WaitForSeconds(2);
+        battleLoader.LoadNextLevel();
         Debug.Log("EMPIEZA BATALLA");
     }
 }
