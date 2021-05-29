3684fefeafa74103617671f5a8a0ab55627c31be
Ismael Baena <ismael.baena.7e3@itb.cat>
2021-05-29 16:53:33 +0200

    Commit

---
+++ Assets/Scripts/Characters/Abilities/CardSet.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "CardSet")]
+public class CardSet : ScriptableObject {
+    public List<GameObject> cards;
+    public int forceValue;
+}
